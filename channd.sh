20f8a0ba44a3b0c73d64b392bd556ba650946eca
to-go-star <94597202+to-go-star@users.noreply.github.com>
2022-01-09 00:39:45 +0800

    Update channd.sh

--- channd.sh
+++ channd.sh
@@ -5,12 +5,12 @@ input=y
 while [[ "$input" == "y" ]]
 do
     result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/70143836" 2>&1)
-    region=$(curl --user-agent "${UA_Browser}" -s --max-time 10 -X POST "https://global.edge.bamgrid.com/devices" -H "authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"  2>&1)
+    region=$(curl  --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36" -s --max-time 10 -X POST "https://global.edge.bamgrid.com/devices" -H "authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"  2>&1)
  if [[ "$result" == "200" ]] &&[[ "$region" =~ "ClientError" ]]; then
-        echo -e "$result$regionIP OK!$(date)"
+        echo -e "$result$region，IP OK!$(date)"
         sleep 1200           
  else
-       echo -e "$result$regionIP OK!$(date)IP掉了切换中$(date)"
+       echo -e "$result$region,IP掉了切换中$(date)"
        systemctl restart wg-quick@wgcf
        sleep 3
   fi
