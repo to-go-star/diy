7150f20b1b9000ebaa771ce7b218e896e3229a1d
to-go-star <94597202+to-go-star@users.noreply.github.com>
2022-02-18 21:28:56 +0800

    Update disney.sh

--- disney.sh
+++ disney.sh
@@ -7,7 +7,8 @@ do
    restwo=$(curl  --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null -s --max-time 5 -X POST "https://global.edge.bamgrid.com/devices" -H "authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84" 2>&1)
  if [[ "$restwo" != "403" ]]; then
         echo -e "$restwo,DISNEY IP is OK!$(date)"
-        sleep 600           
+        systemctl restart wg-quick@wgcf
+        sleep 300           
  else
        echo -e "$result,$restwo,IP废了,切换中!!!$(date)"
        systemctl restart wg-quick@wgcf
