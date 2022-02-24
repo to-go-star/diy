dbeeda94926b0792345d7dd68d863ebcd1a97ec0
to-go-star <94597202+to-go-star@users.noreply.github.com>
2022-02-24 16:44:15 +0800

    Update disney.sh

--- disney.sh
+++ disney.sh
@@ -10,14 +10,6 @@ do
  PreDisneyCookie=$(curl -s --max-time 10 "https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies" | sed -n '1p')
  disneycookie=$(echo $PreDisneyCookie | sed "s/DISNEYASSERTION/${assertion}/g")
  TokenContent=$(curl  --user-agent "${UA_Browser}" -s --max-time 10 -X POST "https://global.edge.bamgrid.com/token" -H "authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84" -d "$disneycookie")
- isBanned=$(echo $TokenContent | python3 -m json.tool 2> /dev/null | grep 'forbidden-location')
- is403=$(echo $TokenContent | grep '403 ERROR')
-
-        if [ -n "$isBanned" ] || [ -n "$is403" ]; then
-            systemctl restart wg-quick@wgcf
-            echo -n -e " $isBanned  $is403 IP废了,切换中!!!"
-            sleep 3
-        fi
  fakecontent=$(curl -s --max-time 10 "https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies" | sed -n '8p')
  refreshToken=$(echo $TokenContent | python3 -m json.tool 2> /dev/null | grep 'refresh_token' | awk '{print $2}' | cut -f2 -d'"')
  disneycontent=$(echo $fakecontent | sed "s/ILOVEDISNEY/${refreshToken}/g")
