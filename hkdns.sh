dd03ce593270d2e6b0b22db0f2ee729ad87c7f76
to-go-star <94597202+to-go-star@users.noreply.github.com>
2022-03-29 20:43:38 +0800

    Create hkdns.sh

---
+++ hkdns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+input=y
+while [[ "$input" == "y" ]]
+do
+    ip=$( ping china-hk1.nfdns.top -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
+    echo -e "domain-needed\nbogus-priv\nno-resolv\nno-poll\nall-servers\nserver=8.8.8.8\nserver=1.1.1.1\nserver=208.67.222.222\nserver=4.2.2.1\ncache-size=2048\nlocal-ttl=60\ninterface=*\naddress=/akadns.net/$ip\naddress=/akam.net/$ip\naddress=/akamai.com/$ip\naddress=/akamai.net/$ip\naddress=/akamaiedge.net/$ip\naddress=/akamaihd.net/$ip\naddress=/akamaistream.net/$ip\naddress=/akamaitech.net/$ip\naddress=/akamaitechnologies.com/$ip\naddress=/akamaitechnologies.fr/$ip\naddress=/akamaized.net/$ip\naddress=/edgekey.net/$ip\naddress=/edgesuite.net/$ip\naddress=/srip.net/$ip\naddress=/level3.net/$ip\naddress=/llnwd.net/$ip\naddress=/edgecastcdn.net/$ip\naddress=/cloudfront.net/$ip\naddress=/netflix.com/$ip\naddress=/netflix.net/$ip\naddress=/nflximg.net/$ip\naddress=/nflxvideo.net/$ip\naddress=/nflxso.net/$ip\naddress=/nflxext.com/$ip\naddress=/oculus.com/$ip\naddress=/ip2location.com/$ip\naddress=/warnerbros.com/$ip\naddress=/southpark.cc.com/$ip\naddress=/brightcove.com/$ip\naddress=/uplynk.com/$ip\naddress=/maxmind.com/$ip\naddress=/braze.com/$ip\naddress=/disney.com/$ip\naddress=/disneyjunior.com/$ip\naddress=/disney-plus.net/$ip\naddress=/disneyplus.com/$ip\naddress=/dssott.com/$ip\naddress=/cdn.registerdisney.go.com/$ip\naddress=/global.edge.bamgrid.com/$ip\naddress=/bamgrid.com/$ip\naddress=/registerdisney.go.com/$ip\naddress=/disneyplus.com.ssl.sc.omtrdc.net/$ip\naddress=/dssott.com.akamaized.net/$ip\naddress=/braze.com/$ip\naddress=/cws.conviva.com/$ip\naddress=/execute-api.us-east-1.amazonaws.com/$ip\naddress=/go-mpulse.net/$ip\naddress=/disneynow.com/$ip\naddress=/search-api-disney.bamgrid.com/$ip\naddress=/d9.flashtalking.com/$ip\naddress=/disney-portal.my.onetrust.com/$ip\naddress=/disneyplus.bn5x.net/$ip\naddress=/adobedtm.com/$ip\naddress=/optimizely.com/$ip\naddress=/disneystreaming.com/$ip\naddress=/com.disney.disneyplus/$ip\naddress=/go-disneyworldgo.com/$ip\naddress=/go.com/$ip\naddress=/thestationbymaker.com/$ip\naddress=/thisispolaris.com/$ip\naddress=/watchdisneyfe.com/$ip
+"  > /etc/dnsmasq.d/custom_netflix.conf
+    echo -e  "当前IP:$ip 时间：$(date)"
+     systemctl restart dnsmasq  
+     sleep 30
+done
